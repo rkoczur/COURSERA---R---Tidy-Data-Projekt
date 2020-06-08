9c2a2ad2989d32e8e9186127467b42b24c35d521
rkoczur <kocz@kocz.hu>
2020-06-08 20:49:17 +0200

    I have realized, that subject and activity stored in separate txt-s (It took a while to fully understand the whole thing). So I have to implement these datas into the read process

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,25 @@
 #Read the test and training datasets into memory and bind together
 
-myDataset <- rbind(cbind(activity = "test", read.table("./data/test/X_test.txt", fill=TRUE)), 
-                   cbind(activity = "train", read.table("./data/train/X_train.txt", fill=TRUE)))
+testSubjects <- read.table("./data/test/subject_test.txt", fill=TRUE)
+trainSubjects <- read.table("./data/train/subject_train.txt", fill=TRUE)
+
+testActivity <- read.table("./data/test/y_test.txt", fill=TRUE)
+trainActivity <- read.table("./data/train/y_train.txt", fill=TRUE)
+
+testData <- cbind(type = "test", subject = testSubjects, activity = testActivity, read.table("./data/test/X_test.txt", fill=TRUE))
+trainData <- cbind(type = "train", subject = trainSubjects, activity = trainActivity, read.table("./data/train/X_train.txt", fill=TRUE))
+
+myDataset <- rbind(testData, trainData)
+
+#Free up memory
+
+rm(list=setdiff(ls(), "myDataset"))
 
 #Name the variables according to features.txt
 
-names(myDataset) <- c("activity", as.vector(read.table("./data/features.txt", fill=TRUE)[,2]))
+names(myDataset) <- c("obsType", "subject", "activity", as.vector(read.table("./data/features.txt", fill=TRUE)[,2]))
 
 #Keep only the variables with mean and standard deviaton values (and of course the activity type)
 
-myDataset <- myDataset[, grep("mean\\(\\)|std\\(\\)|type", names(myDataset))]
+myDataset <- myDataset[, c(1:3, grep("mean\\(\\)|std\\(\\)", names(myDataset)))]
 
